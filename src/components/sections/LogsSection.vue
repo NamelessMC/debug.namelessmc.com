67b8d36041b0bc3e115d3e31ba2a3e63207ced3c
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-04-28 11:45:52 -0700

    Fix logs on mobile

--- src/components/sections/LogsSection.vue
+++ src/components/sections/LogsSection.vue
@@ -3,11 +3,11 @@
     <div class="section-title">
       <font-awesome-icon icon="file-pen"/> {{ $t('logs_section.logs') }}
     </div>
-    <div class="section-content">
+    <div class="section-content grid grid-cols-1 gap-y-4">
       <div v-for="(content, type) of data.logs" :key="type">
         <p class="text-normal text-left pl-4 font-medium">{{ $t(`logs_section.${type}_log`) }}</p>
         <highlightjs
-            class="log-content mb-6"
+            class="log-content"
             language="log"
             :code="content ? content : $t('logs_section.no_logs')"
         />
