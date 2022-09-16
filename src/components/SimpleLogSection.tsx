abb18e9ae72db5bfe59d1abf125639f48072efd1
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-09-15 19:13:01 -0600

    fixup log syntax highlighter

--- src/components/SimpleLogSection.tsx
+++ src/components/SimpleLogSection.tsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {IconDefinition} from "@fortawesome/free-solid-svg-icons";
 import {useTranslation} from "react-i18next";
-import Highlight from "react-highlight";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import {atomOneDark, atomOneLight} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 interface Props {
     icon: IconDefinition;
@@ -20,6 +21,10 @@ function SimpleLogSection({
 
     const { t } = useTranslation();
 
+    const theme = localStorage.getItem('nmc-debug-theme') ?? 'light'
+
+    // would be great to use original github dark theme from vue version, but these are fine for now
+
     return (
         <div className="section-background">
             <div className="section-title">
@@ -29,7 +34,9 @@ function SimpleLogSection({
                 {logs.map((log, idx) => (
                     <div key={idx}>
                         <p className="text-normal text-left pl-4 font-medium">{ t(log.titleKey) }</p>
-                        <Highlight className='log-content log'>{log.content.length > 0 ? log.content : t('logs_section.empty_log')}</Highlight>
+                        <SyntaxHighlighter className="log-content log" language="accesslog" wrapLongLines={true} style={theme === 'light' ? atomOneLight : atomOneDark}>
+                            {log.content.length > 0 ? log.content : t('logs_section.empty_log')}
+                        </SyntaxHighlighter>
                     </div>
                 ))}
             </div>
