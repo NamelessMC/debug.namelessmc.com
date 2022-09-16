ac783d5df41b67be87887bcaf1b426eb16351d3a
Tadhg Boyle <tadhgsmboyle@gmail.com>
2022-09-15 19:50:30 -0600

    fix loader icon

--- src/components/includes/Loader.tsx
+++ src/components/includes/Loader.tsx
@@ -1,34 +1,19 @@
 import { useTranslation } from 'react-i18next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-interface Props {
-    loaded: boolean;
-    error: boolean;
-}
-
-function Loader({
-    loaded,
-    error
-}: Props) {
+function Loader() {
 
     const { t } = useTranslation();
 
-    // Should these checks be moved to the parent component?
-    if (error) return <></>;
-
     return (
-        <div>
-            { !loaded &&
-                <div
-                    className="m-auto rounded-lg shadow-lg md:w-1/4 p-4 bg-gray-300 dark:bg-gray-500 mb-2 font-bold text-white"
-                >
-                    { t('misc.loading') }
-                    <span className="float-right">
-                        <FontAwesomeIcon icon={faCog} className="fa-spin" />
-                    </span>
-                </div>
-            }
+        <div
+            className="m-auto rounded-lg shadow-lg md:w-1/4 p-4 bg-gray-300 dark:bg-gray-500 mb-2 font-bold text-white"
+        >
+            { t('misc.loading') }
+            <span className="float-right">
+                <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
+            </span>
         </div>
     )
 }
